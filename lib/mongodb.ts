4073b598000a8a935e2b6a90cc44ee87aaf00513
Favourite <favourson71@gmail.com>
2026-09-15 04:12:43 +0100

    fix: lazy initialize MongoDB for CI

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -3,12 +3,6 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 import { attachDatabasePool } from "@vercel/functions";
 
-const uri = process.env.MONGODB_URI;
-
-if (!uri) {
-    throw new Error("Please define MONGODB_URI in your environment variables.");
-}
-
 const options = {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -16,22 +10,35 @@ const options = {
         deprecationErrors: true,
     },
 
-    // Close idle connections relatively quickly on Vercel
     maxIdleTimeMS: 5_000,
 };
 
 declare global {
-    var _mongoClient: MongoClient | undefined;
+    var _mongoClientPromise: Promise<MongoClient> | undefined;
 }
 
-const client =
-    global._mongoClient ??
-    new MongoClient(uri, options);
+function createMongoClient(): Promise<MongoClient> {
+    const uri = process.env.MONGODB_URI;
 
-if (!global._mongoClient) {
-    global._mongoClient = client;
+    if (!uri) {
+        throw new Error(
+            "Please define MONGODB_URI in your environment variables."
+        );
+    }
+
+    const client = new MongoClient(uri, options);
+
+    attachDatabasePool(client);
+
+    return client.connect();
 }
 
-attachDatabasePool(client);
+if (!global._mongoClientPromise) {
+    global._mongoClientPromise = Promise.resolve().then(
+        createMongoClient
+    );
+}
 
-export default client;
+const clientPromise = global._mongoClientPromise;
+
+export default clientPromise;
